29643f7f4d5a40710d602bb6eb6956a2543de7e4
Renée Kooi <renee@kooi.me>
2020-01-22 16:39:15 +0100

    use a const enum for internal token types

--- src/index.ts
+++ src/index.ts
@@ -81,13 +81,24 @@ function escapeStringRegExp(str: string) {
   return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
 }
 
+const enum TokenType {
+  Italic,
+  Bold,
+  Code,
+  Strike,
+  Emoji,
+  Mention,
+  Link,
+  Text,
+};
+
 interface Token {
-  type: string;
+  type: TokenType;
   text: string;
   raw: string;
 };
 
-function createToken(type: string, text: string, raw: string = text): Token {
+function createToken(type: TokenType, text: string, raw: string = text): Token {
   return { type, text, raw};
 }
 
@@ -136,7 +147,7 @@ function tokenize(text: string, options: MarkupOptions) {
   const tokens: Token[] = [];
   // adds a token of type `type` if the current chunk starts with
   // a `delim`-delimited string
-  const delimited = (start: string, endRx: RegExp, type: string) => {
+  const delimited = (start: string, endRx: RegExp, type: TokenType) => {
     if (chunk[0] === start && chunk[1] !== start) {
       const end = 1 + chunk.slice(1).search(endRx);
       if (end) {
@@ -147,7 +158,7 @@ function tokenize(text: string, options: MarkupOptions) {
     }
     return false;
   };
-  const emoji = (type: string, emojiNames?: string[]) => {
+  const emoji = (type: TokenType, emojiNames?: string[]) => {
     const match = /^:([A-Za-z0-9_+-]+):/.exec(chunk);
     if (match) {
       // if a whitelist of emoji names is given, only accept emoji from that
@@ -161,7 +172,7 @@ function tokenize(text: string, options: MarkupOptions) {
     }
     return false;
   };
-  const mention = (start: string, type: string) => {
+  const mention = (start: string, type: TokenType) => {
     if (chunk[0] === start) {
       const maybeMention = chunk.slice(1);
       for (let mi = 0, ml = mentions.length; mi < ml; mi += 1) {
@@ -177,7 +188,7 @@ function tokenize(text: string, options: MarkupOptions) {
     return false;
   };
   const linkRx = new RegExp(`^${urlRegExp().source}`, 'i');
-  const link = (type: string) => {
+  const link = (type: TokenType) => {
     const match = linkRx.exec(chunk);
     if (match) {
       tokens.push(createToken(type, chunk.slice(0, match[0].length)));
@@ -191,30 +202,30 @@ function tokenize(text: string, options: MarkupOptions) {
     // .slice again because `i` changed
     const m = /^\s+/.exec(text.slice(i));
     if (m) {
-      tokens.push(createToken('word', m[0]));
+      tokens.push(createToken(TokenType.Text, m[0]));
       i += m[0].length;
     }
   };
   // tokenize text, just loop until it's done!
   chunk = text;
   while (chunk) {
-    const found = emoji('emoji', options.emojiNames)
-      || delimited('_', /_(\W|$)/, 'italic')
-      || delimited('*', /\*(\W|$)/, 'bold')
-      || delimited('`', /`(\W|$)/, 'code')
-      || delimited('~', /~(\W|$)/, 'strike')
-      || mention('@', 'mention')
-      || link('link');
+    const found = emoji(TokenType.Emoji, options.emojiNames)
+      || delimited('_', /_(\W|$)/, TokenType.Italic)
+      || delimited('*', /\*(\W|$)/, TokenType.Bold)
+      || delimited('`', /`(\W|$)/, TokenType.Code)
+      || delimited('~', /~(\W|$)/, TokenType.Strike)
+      || mention('@', TokenType.Mention)
+      || link(TokenType.Link);
     if (!found) {
       let end = chunk.indexOf(' ', 1) + /* eat space */ 1;
       if (end === 0) { // no match, = -1 + 1
         end = chunk.length;
       }
-      // append to previous token if it was also a word
-      if (tokens.length > 0 && tokens[tokens.length - 1].type === 'word') {
+      // append to previous token if it was also text
+      if (tokens.length > 0 && tokens[tokens.length - 1].type === TokenType.Text) {
         tokens[tokens.length - 1].text += chunk.slice(0, end);
       } else {
-        tokens.push(createToken('word', chunk.slice(0, end)));
+        tokens.push(createToken(TokenType.Text, chunk.slice(0, end)));
       }
       i += end;
     }
@@ -241,21 +252,21 @@ export default function parse(message: string, options: MarkupOptions = {}): Mar
 
   return tokenize(message, options).map((token) => {
     switch (token.type) {
-      case 'italic':
+      case TokenType.Italic:
         return { type: 'italic', content: parse(token.text, options) };
-      case 'bold':
+      case TokenType.Bold:
         return { type: 'bold', content: parse(token.text, options) };
-      case 'code':
+      case TokenType.Code:
         return { type: 'code', content: [token.text] };
-      case 'strike':
+      case TokenType.Strike:
         return { type: 'strike', content: parse(token.text, options) };
-      case 'emoji':
+      case TokenType.Emoji:
         return { type: 'emoji', name: token.text };
-      case 'mention':
+      case TokenType.Mention:
         return { type: 'mention', mention: token.text.toLowerCase(), raw: token.text };
-      case 'link':
+      case TokenType.Link:
         return { type: 'link', text: token.text, href: httpify(token.text) };
-      default:
+      case TokenType.Text:
         return token.text;
     }
   });
